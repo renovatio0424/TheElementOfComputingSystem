ee7c2239983ba2b1665ea07c7688240302d26468
reno-kim-km <reno.kim@kinemaster.com>
2020-01-15 21:58:16 +0900

    04. mult.hack

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,39 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+	@i
+	M=1
+	@sum
+	M=0
+(LOOP)
+	@i	//D=i
+	D=M
+	@1	//D=i-RAM[1]
+	D=D-A
+	@END
+	D;JGT	// If(i-RAM[1])=0 goto END
+	
+	// i += 1
+	@i
+	M=M+1
+
+	// sum += RAM[0]
+	@sum
+	D=M
+	@0
+	D=D+M
+	@sum
+	M=D
+
+	// jump to loop
+	@LOOP
+	0;JMP
+(END)
+	@sum
+	D=M
+	@2
+	M=D
+
+	@END
+	0;JMP
+
