e11efd87885ae2f2c51fc38527aef23395fec97d
renovatio0424 <drkstage@naver.com>
2020-01-16 03:09:23 +0900

    update multiply

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,39 +7,47 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
-	@i
-	M=1
-	@sum
-	M=0
-(LOOP)
-	@i	//D=i
-	D=M
-	@1	//D=i-RAM[1]
-	D=D-A
-	@END
-	D;JGT	// If(i-RAM[1])=0 goto END
-	
-	// i += 1
-	@i
-	M=M+1
 
-	// sum += RAM[0]
-	@sum
-	D=M
-	@0
-	D=D+M
-	@sum
-	M=D
+//i=R0
+@R0
+D=M
+@i
+M=D        
 
-	// jump to loop
-	@LOOP
+//j=R1
+@R1
+D=M
+@j
+M=D    
+
+//Mult = 0
+@Mult
+M=0    
+
+(LOOP)     
+	//if(i=0) goto END
+	@i
+	D=M
+	@END    
+	D;JLE     
+	//Mult = Mult + j
+	@j
+	D=M
+	@Mult
+	M=M+D
+
+	//if(i-1) = 0 goto LOOP
+	@i
+	M=M-1
+	@LOOP     
 	0;JMP
 (END)
-	@sum
-	D=M
-	@2
-	M=D
+//R2=Mult
+@Mult  
+D=M
+@R2
+M=D        
 
-	@END
-	0;JMP
+@END
+0;JEQ
 
